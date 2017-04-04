caafc93ee79b1d444902036f0f80c8000bb539e3
Nicole Hurley <nicoleleehurley@gmail.com>
2017-04-04 15:02:53 -0400

    Scene Change
    changes scene when you run into the goblin, currently just switches to
    an identical scene called “level02” but this will be the combat scene
    after we make it

---
+++ UntitledGame/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	void OnTriggerEnter(Collider ChangeScene) {
+		if(ChangeScene.gameObject.CompareTag("Player"))
+		{
+			Application.LoadLevel ("level02");
+		}
+	}
+}
+
