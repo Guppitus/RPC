c31f3c627bb419d4edba403670cd9b3890b64206
Guppitus <Seth.lunn90@gmail.com>
2017-04-04 15:25:14 -0400

    what the fuck wumbos

--- UntitledGame/Assets/Scripts/LevelManager.cs
+++ UntitledGame/Assets/Scripts/LevelManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
+public class Level_Manager : MonoBehaviour {
 
     public Transform mainMenu, optionsMenu;
 
