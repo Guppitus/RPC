51b9cb5a85367a1214a91aed4820296e9cdcf008
Nicole Hurley <nicoleleehurley@gmail.com>
2017-04-05 14:53:32 -0400

    sword changes....etc

--- RPC_Demo/Assets/_CompletedAssets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ RPC_Demo/Assets/_CompletedAssets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,17 +12,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
+		private Animator anim;
         
         private void Start()
         {
             // get the transform of the main camera
-            if (Camera.main != null)
-            {
+            if (Camera.main != null) {
                 m_Cam = Camera.main.transform;
-            }
-            else
-            {
+            } else {
                 Debug.LogWarning(
                     "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.");
                 // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
@@ -30,46 +27,58 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
+
         }
 
 
         private void Update()
         {
-            if (!m_Jump)
-            {
+            if (!m_Jump) {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
+		 
         }
 
 
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+			if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "level01") {
+				
+				// read inputs
+				float h = CrossPlatformInputManager.GetAxis ("Horizontal");
+				float v = CrossPlatformInputManager.GetAxis ("Vertical");
+				bool crouch = Input.GetKey (KeyCode.C);
 
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
-            }
+				// calculate move direction to pass to character
+				if (m_Cam != null) {
+					// calculate camera relative direction to move:
+					m_CamForward = Vector3.Scale (m_Cam.forward, new Vector3 (1, 0, 1)).normalized;
+					m_Move = v * m_CamForward + h * m_Cam.right;
+				} else {
+					// we use world-relative directions in the case of no main camera
+					m_Move = v * Vector3.forward + h * Vector3.right;
+				}
 #if !MOBILE_INPUT
-			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+				// walk speed multiplier
+				if (Input.GetKey (KeyCode.LeftShift))
+					m_Move *= 0.5f;
 #endif
 
-            // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
-            m_Jump = false;
+				// pass all parameters to the character control script
+				m_Character.Move (m_Move, crouch, m_Jump);
+				m_Jump = false;
+			} else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "level02"){
+				float h = CrossPlatformInputManager.GetAxis ("Horizontal");
+				float v = CrossPlatformInputManager.GetAxis ("Vertical");
+
+				m_Move = v * Vector3.one + h * Vector3.one;
+				m_Character.Move (m_Move, false, false);
+
+
+				anim = GetComponent<Animator> ();
+				anim.SetBool ("InCombat", true);
+			}
         }
     }
 }
