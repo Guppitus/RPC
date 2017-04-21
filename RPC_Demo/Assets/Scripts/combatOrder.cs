bc2582b3cc98c8af042c521b546321ce8e078deb
Guppitus <Seth.lunn90@gmail.com>
2017-04-21 12:17:02 -0400

    updated slots ui

---
+++ RPC_Demo/Assets/Scripts/combatOrder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class combatOrder : MonoBehaviour, IHasChanged {
+    [SerializeField] Transform slots;
+
+
+    public void HasChanged()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        builder.Append(" -> ");
+        foreach(Transform slotTransform in slots)
+        {
+          //  GameObject item = slotTransform.GetComponent<Slot>().item;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start() {
+        HasChanged();
+    }
+}
+
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
+
