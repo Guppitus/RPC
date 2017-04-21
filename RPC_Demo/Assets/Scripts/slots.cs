7b2baff18f420c7810932ff7607885d5e66de11d
guppitus <seth.lunn90@gmail.com>
2017-04-20 21:29:03 -0400

    Drag and drop working

---
+++ RPC_Demo/Assets/Scripts/slots.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class slots : MonoBehaviour, IDropHandler {
+
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild (0).gameObject;
+
+            }
+            return null;
+        }
+
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandler.itemBeingDragged.transform.SetParent(transform);
+        }
+    }
+}
